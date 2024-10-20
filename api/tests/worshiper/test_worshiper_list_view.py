aeac07db2a4a6248963d55ecff1f1f876f33933e
goro takeda <rugbytkg3@gmail.com>
2024-10-20 18:14:47 +0900

    テストコード修正

--- api/tests/worshiper/test_worshiper_list_view.py
+++ api/tests/worshiper/test_worshiper_list_view.py
@@ -10,9 +10,9 @@ django.setup()
 
 class WorshiperListViewTest(TestCase):
     def setUp(self):
-        self.worshipers = WorshiperFactory.create_batch(3)
+        self.worshipers = WorshiperFactory.create_batch(5)
 
     def test_get(self):
         response = self.client.get(reverse("worshiper_list"))
-        assert len(response.data) == 3
+        assert len(response.data) == 5
         assert response.status_code == 200
