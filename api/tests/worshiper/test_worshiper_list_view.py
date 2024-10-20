1d0dc18cc99cabf628bc0cc1b07c6436adea157e
goro takeda <rugbytkg3@gmail.com>
2024-10-20 18:24:01 +0900

    テストコード修正

--- api/tests/worshiper/test_worshiper_list_view.py
+++ api/tests/worshiper/test_worshiper_list_view.py
@@ -1,7 +1,11 @@
 import os
+import sys
 import django
 from django.test import TestCase
 from django.urls import reverse
+
+# プロジェクトのルートディレクトリをパスに追加
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
 from api.tests.factories.worshiper_factory import WorshiperFactory
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jinja_backend.settings")
