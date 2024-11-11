a5f46259f3d940420e63f1bd519ffa45f7a8e931
goro takeda <rugbytkg3@gmail.com>
2024-11-12 02:17:34 +0900

    管理者ログインAPIの実装

--- jinja_backend/urls.py
+++ jinja_backend/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from api.views.worshiper.worshiper_list_view import WorshiperListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/worshipers/", WorshiperListView.as_view(), name="worshiper_list"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
